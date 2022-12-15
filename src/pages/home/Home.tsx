632cff6dde08a4d3a297b6c8d8d2b5c0f5d4a764
xie <shie0041@gmail.com>
2022-12-15 18:28:04 +0900

    a

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,66 @@
+import { styled } from '@mui/material/styles'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 // ヘッダー、フッターの部分
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}))
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+    }))
+
+function createData(
+    name: string
+) {
+    return {name}
+}
+
+const rows = [
+    createData('白米'),
+    createData('ヨーグルト'),
+    createData('アイスコーヒー'),
+    createData('卵'),
+    createData('フルーツ イチゴ'),
+]
 export default function Home() {
+
     return (
-        <header>
-            <h1 id="title">1日の食費がわかる!</h1>
-            <input placeholder="食材を検索" />
-            <button>検索</button>
-        </header>
+        <TableContainer component={Paper}>
+        <Table>
+        <caption>＋ 登録</caption>
+            <TableHead>
+            <TableRow>
+                <StyledTableCell>朝食</StyledTableCell> 
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                    <StyledTableCell component='th' scope='row'>
+                    {row.name}
+                </StyledTableCell>
+                </StyledTableRow>
+                ))}
+            </TableBody>
+            </Table>
+        </TableContainer>
         )
 }
