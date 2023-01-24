4c5bb7f89622392fa52d6e647700835d3362f810
xie <shie0041@gmail.com>
2023-01-24 16:33:11 +0900

    完成！

--- src/components/MealDashboard.tsx
+++ src/components/MealDashboard.tsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
-import { Button, Grid } from "@mui/material";
-import { CheckItem } from "../models/CheckItem";
-import { Item } from "../models/Item";
-import { SearchDialog } from "../pages/searchDialog/SearchDialog";
-import products from "../data/food-expenses.json";
+import { useState } from 'react'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles'
+import { Button, Grid } from '@mui/material'
+import { CheckItem } from '../models/CheckItem'
+import { Item } from '../models/Item'
+import { SearchDialog } from '../pages/searchDialog/SearchDialog'
+import products from '../data/food-expenses.json'
 
 //テーブルのレイアウト
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -22,111 +22,117 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
   },
-}));
+}))
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
+  '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
   },
-  "&:last-child td, &:last-child th": {
+  '&:last-child td, &:last-child th': {
     border: 0,
   },
-}));
+}))
+
+type sF = (mealPrice: number) => void
 
 interface props {
-  title :string
+  title: string
+  setMealPrice: sF
 }
 
-export function MealDashboard({title}: props) {
+export function MealDashboard({ title, setMealPrice }: props) {
   //初めのテーブルアイテムはfalseで表示
   const addChecked = (product: Item) => {
     const newProduct: CheckItem = {
       ...product,
       ...{ checked: false },
-    };
-    return newProduct;
-  };
+    }
+    return newProduct
+  }
   //falseにしたproductsをmapする
-  const newProducts: CheckItem[] = products.map(addChecked);
+  const newProducts: CheckItem[] = products.map(addChecked)
 
   //初期値はmapしたfalseのproducts
-  const [tableItem, setTableItem] = useState<CheckItem[]>(newProducts);
-  
+  const [tableItem, setTableItem] = useState<CheckItem[]>(newProducts)
+
   //新しいリストアイテム（更新ボタンを押したときにはtrueに変わってる）をテーブルに追加していく
   function upDate(newItems: CheckItem[]) {
-    setTableItem(newItems);
+    setTableItem(newItems)
   }
   //Dialogを開く
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
   const handleClickOpen = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   //合計金額を計算した値
-  const calcTotalPrice = (tableItem:CheckItem[]) => {
+  const calcTotalPrice = (tableItem: CheckItem[]) => {
     return tableItem
       .filter((item) => {
         return item.checked
       })
       .reduce((sum: number, val: CheckItem): number => {
-        return sum + val.price;
-      }, 0);
+        return sum + val.price
+      }, 0)
   }
-  
+
   //初期値は空の合計金額
-  const [totalPrice, setTotalPrice] = useState(calcTotalPrice(tableItem));
+  const [totalPrice, setTotalPrice] = useState(calcTotalPrice(tableItem))
 
   //合計金額を更新ボタン押されるたびに更新
-  function upDateSum (newTableItem: CheckItem[]) {
+  function upDateSum(newTableItem: CheckItem[]) {
     setTotalPrice(calcTotalPrice(newTableItem))
   }
-  
+  setMealPrice(totalPrice)
+
   return (
     <>
-    <Grid container>
-      <Grid item xs={1} />
-      <Grid item xs={10}>
-        <TableContainer component={Paper}>
-          <Table>
-            <caption>
-              <Button onClick={handleClickOpen}>編集</Button>
-              <SearchDialog
-                open={open}
-                tableItem={tableItem}
-                handleClose={handleClose}
-                upDate={(items) => upDate(items)}
-                upDateSum={(items) => upDateSum(items)} 
-              />
-            </caption>
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>{title}</StyledTableCell>
-                <StyledTableCell align="right">
-                  { `合計 : ${ totalPrice } 円` }
-                </StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {tableItem
-                .filter((item) => {
-                  return item.checked;
-                })
-                .map((item) => (
-                  <StyledTableRow key={item.name}>
-                    <StyledTableCell>{item.name}</StyledTableCell>
-                    <StyledTableCell align="right">
-                      {item.price + " 円"}
-                    </StyledTableCell>
-                  </StyledTableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+      <Grid container>
+        <Grid item xs={1} />
+        <Grid item xs={10}>
+          <TableContainer component={Paper}>
+            <Table>
+              <caption>
+                <Button onClick={handleClickOpen}>編集</Button>
+                <SearchDialog
+                  open={open}
+                  tableItem={tableItem}
+                  handleClose={handleClose}
+                  upDate={(items) => upDate(items)}
+                  upDateSum={(items) => upDateSum(items)}
+                />
+              </caption>
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell sx={{ typography: 'body1' }}>
+                    {title}
+                  </StyledTableCell>
+                  <StyledTableCell align='right' sx={{ typography: 'body1' }}>
+                    {`合計 : ${totalPrice} 円`}
+                  </StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tableItem
+                  .filter((item) => {
+                    return item.checked
+                  })
+                  .map((item) => (
+                    <StyledTableRow key={item.name}>
+                      <StyledTableCell>{item.name}</StyledTableCell>
+                      <StyledTableCell align='right'>
+                        {item.price + ' 円'}
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={1} />
       </Grid>
-      <Grid item xs={1} />
-    </Grid>
     </>
-  );
+  )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,55 +1,65 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Box, Grid, Stack } from "@mui/material";
-import { MealDashboard } from "../../components/MealDashboard"
-import { AccountCircle } from '@mui/icons-material';
+import { useState } from 'react'
+import { AccountCircle } from '@mui/icons-material'
+import AppBar from '@mui/material/AppBar'
+import { Box, Grid, Stack } from '@mui/material'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import { MealDashboard } from '../../components/MealDashboard'
 
 function appBarLabel(label: string) {
   return (
     <Toolbar>
-      <IconButton edge="start" color="inherit" sx={{ mr: 2 }}>
+      <IconButton edge='start' color='inherit' sx={{ mr: 2 }}>
         <MenuIcon />
       </IconButton>
-      <Typography variant="h6">
-        {label}
-      </Typography>
+      <Typography variant='h6'>{label}</Typography>
       <Box sx={{ flexGrow: 1 }} />
       <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-        <IconButton
-          size="large"
-          edge="end"
-          color="inherit"
-        >
+        <IconButton size='large' edge='end' color='inherit'>
           <AccountCircle />
         </IconButton>
       </Box>
     </Toolbar>
-  );
+  )
 }
 
 export default function Home() {
+  const [mealPrice1, setMealPrice1] = useState(0)
+  const [mealPrice2, setMealPrice2] = useState(0)
+  const [mealPrice3, setMealPrice3] = useState(0)
+
+  //３つのテーブルの計算をする
+
   return (
     <>
-    <Stack spacing={5}>
-      <AppBar position="static" color="primary">
-        {appBarLabel('食費管理')}
-      </AppBar>
-      <Grid container alignItems="center" >
-        <Grid item xs={12}>
-            <Typography textAlign="center" component="div" variant="h5">
-            一日の食費　 合計 : 3000 円
+      <Stack spacing={5}>
+        <AppBar position='static' style={{ backgroundColor: '#01579b' }}>
+          {appBarLabel('食費管理')}
+        </AppBar>
+        <Grid container alignItems='center'>
+          <Grid item xs={12}>
+            <Typography textAlign='center' component='div'>
+              一日の食費
             </Typography>
+            <Typography textAlign='center' component='div' variant='h5'>
+              <b>
+                合計 :{' '}
+                <b style={{ color: '#1976D2' }}>
+                  {mealPrice1 + mealPrice2 + mealPrice3}
+                </b>{' '}
+                円
+              </b>
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <Stack direction="row">
-        <MealDashboard title = "朝食"/>
-        <MealDashboard title = "昼食"/>
-        <MealDashboard title = "夕食"/> 
+        <Stack direction='row'>
+          <MealDashboard title='朝食' setMealPrice={setMealPrice1} />
+          <MealDashboard title='昼食' setMealPrice={setMealPrice2} />
+          <MealDashboard title='夕食' setMealPrice={setMealPrice3} />
+        </Stack>
       </Stack>
-    </Stack>
     </>
-  );
+  )
 }
