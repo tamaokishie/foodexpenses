77003b9de3e5496d49afda8660a4f5ae80fc3385
xie <shie0041@gmail.com>
2023-01-25 11:59:52 +0900

    404

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -6,7 +6,7 @@ export default function Routing() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route  path='/foodexpenses/'  element={<Home />} />
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}/>
       </Routes>
     </BrowserRouter>
   )
